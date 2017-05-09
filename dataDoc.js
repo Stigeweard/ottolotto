356e3b8d6dcecf61630bbda944e8560513b93b8c
C S Harding <charliesharding@gmail.com>
2017-05-09 15:35:32 -0500

    work after under 3 hours

---
+++ dataDoc.js
@@ -0,0 +1,33 @@
+let data = {
+    'map': [{
+        'to': 1,
+        'from': 3
+    }, {
+        'to': 3,
+        'from': 2
+    }],
+    'details': {
+        2: {
+            'name': 'Learn To Print',
+            'url': 'http://...'
+        },
+        1: {
+            'name': 'Learn Functions',
+            'url': 'http'
+        },
+        3: {
+            'name': 'Learn The Command Line',
+            'url': 'httpas'
+        }
+    }
+}
+function doctorData(data) {
+    data.map.map((edge)=>{
+        edge.target = data.details[edge.to].name;
+        edge.source = data.details[edge.from].name;
+        edge.url = data.details[edge.from].url;
+        edge.type = 'suit';
+    })
+    return data.map;
+}
+let links = doctorData(data);
---
+++ roadMap.js
@@ -0,0 +1,89 @@
+
+var nodes = {};
+// Compute the distinct nodes from the links.
+links.forEach(function(link) {
+    link.source = nodes[link.source] || (nodes[link.source] = {
+        name: link.source
+    });
+    link.target = nodes[link.target] || (nodes[link.target] = {
+        name: link.target
+    });
+});
+
+var width = 960,
+    height = 500;
+
+var force = d3.layout.force()
+    .nodes(d3.values(nodes))
+    .links(links)
+    .size([width, height])
+    // refactor distance to be dependent on number of nodes
+    .linkDistance(120)
+    .charge(-800)
+    .on("tick", tick)
+    .start();
+
+var svg = d3.select("#chartDiv").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// Per-type markers, as they don't inherit styles.
+svg.append("defs").selectAll("marker")
+    .data(["suit", "licensing", "resolved"])
+    .enter().append("marker")
+    .attr("id", function(d) {
+        return d;
+    })
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 25)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 6)
+    // adjusts arrow
+    .attr("markerHeight", 60)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M0,-5L10,0L0,5");
+
+var path = svg.append("g").selectAll("path")
+    .data(force.links())
+    .enter().append("path")
+    .attr("class", function(d) {
+        return "link " + d.type;
+    })
+    .attr("marker-end", function(d) {
+        return "url(#" + d.type + ")";
+    });
+
+var circle = svg.append("g").selectAll("circle")
+    .data(force.nodes())
+    .enter().append("circle")
+    .attr("r", 14)
+    .call(force.drag);
+
+var text = svg.append("g").selectAll("text")
+    .data(force.nodes())
+    .enter().append("text")
+    //adjusts text placement, adjust size in css
+    .attr("x", 20)
+    .attr("y", ".31em")
+    .text(function(d) {
+        return d.name;
+    });
+
+// Use elliptical arc path segments to doubly-encode directionality.
+function tick() {
+    path.attr("d", linkArc);
+    circle.attr("transform", transform);
+    text.attr("transform", transform);
+}
+
+function linkArc(d) {
+    var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = Math.sqrt((dx * dx + dy * dy)*15);
+    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+}
+
+function transform(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+}
